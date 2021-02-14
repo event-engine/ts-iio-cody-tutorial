cf276ee705b7b3ff89dc4d50273089cf26b62444
codeliner <kontakt@codeliner.ws>
2021-02-14 20:57:22 +0100

    Check aggregate directory first

--- exercises/exercise3.ts
+++ exercises/exercise3.ts
@@ -1,6 +1,8 @@
 import * as fs from "fs";
 
 test("Add addBuilding(command: AddBuilding) function in exercises/src/Aggregate/Building", () => {
+    expect(fs.existsSync('src/Aggregate/Building')).toBe(true);
+
     expect(fs.existsSync('src/Aggregate/Building/addBuilding.ts')).toBe(true);
 
     const content = fs.readFileSync('src/Aggregate/Building/addBuilding.ts', 'utf8');
