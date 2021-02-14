124108597407f613ecec7ae7715217b42b140357
codeliner <kontakt@codeliner.ws>
2021-02-14 19:39:28 +0100

    Add exercise II

--- exercises/exercise2.ts
+++ exercises/exercise2.ts
@@ -1,3 +1,15 @@
+import * as fs from "fs";
+
 test("Add buildingId (string) and name (string) properties to AddBuilding", () => {
-    expect(false).toBe(true);
+    expect(fs.existsSync('src/Command/AddBuilding.ts')).toBe(true);
+
+    const commandContent = fs.readFileSync('src/Command/AddBuilding.ts', 'utf8');
+
+    expect(commandContent).toBe(
+`
+export interface AddBuilding {
+  buildingId: string;
+  name: string;
+}
+`);
 })
